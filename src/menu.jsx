baeb4488839ded97fc68619b553505a4ca6524eb
mahsa reshadi <mahsa@reshadi.com>
2021-05-04 00:52:29 -0700

    third-view

--- src/menu.jsx
+++ src/menu.jsx
@@ -10,7 +10,7 @@ function SetVal (props){
 export function Menu(){
     return(
         <div id="lmenu">
-        <SetVal link="#l1"><Vertical vertical="Mahsa Reshadi"/></SetVal>
+        <SetVal link="#About"><Vertical vertical="Mahsa Reshadi"/></SetVal>
         <SetVal link="https://github.com/mahsareshadi"><img className="social-media-logo" src="./assets/icon/github.png" alt="png"/></SetVal>
         <SetVal link="https://www.linkedin.com/in/mahsa-reshadi-44646620b/"><img className="social-media-logo" src="./assets/icon/linkedin.png" alt="png"/></SetVal>
         <SetVal link="https://twitter.com/mahsareshadi"><img className="social-media-logo" src="./assets/icon/twitter.png" alt="png"/></SetVal>
--- src/nav.jsx
+++ src/nav.jsx
@@ -11,13 +11,11 @@ export function Navigation() {
     return (
         <div id="nav-links">
             <ul className="list">
-                <Nav link="about"><b>About</b></Nav>
+                <Nav link="About"><b>About</b></Nav>
                 <Nav link="Projects"><b>Projects</b></Nav>
                 <Nav link="Education"><b>Education</b></Nav>
-                <Nav link="Skils"><b>Skils</b></Nav>
+                <Nav link="Skills"><b>Skills</b></Nav>
                 <Nav link="Contact"><b>Contact</b></Nav>
-
-                
             </ul>
         </div>
     )
