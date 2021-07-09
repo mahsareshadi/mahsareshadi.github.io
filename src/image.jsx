43a5c3d9b57dd7224469564058472a407699ebf2
mahsa reshadi <mahsa@reshadi.com>
2021-07-08 17:54:46 -0700

    change photo

--- src/image.jsx
+++ src/image.jsx
@@ -1,8 +1,8 @@
 import "./image.css"
 function Image() {
     return (
-        <>
+        <div id="test">
             <img id="my-img" src="/assets/image/img31.jpg" alt="my-pic"/>
-        </>);
+        </div>);
 }
 export default Image;
